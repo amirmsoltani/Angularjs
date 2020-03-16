d3b35a55e57d611766c39f3e03dc6f30dc290645
amir <amirmohhamadsoltani@gmail.com>
2020-03-16 17:07:31 +0330

    dropdown component backend end process

--- Components/CostumeDropDown/index.js
+++ Components/CostumeDropDown/index.js
@@ -8,25 +8,37 @@ costumeDropDown.controller('costumeDropDown', function ($scope) {
     ctrl.pointer = -1;
     ctrl.input = '';
     ctrl.mainId = 'main' + Math.floor(Math.random() * 0xffff).toString();
-    this.removeListen = function () {
+    this.removeListen = function (a) {
         ctrl.show = false;
         window.removeEventListener('click', ctrl.click);
         window.removeEventListener('keydown', ctrl.keyDown);
-        $scope.$apply();
+        if (!a)
+            $scope.$apply();
     };
+    //user focus text box
     this.focus = function () {
         ctrl.show = true;
         window.addEventListener('click', ctrl.click);
         window.addEventListener('keydown', ctrl.keyDown);
     };
+    // user click object
     this.click = function () {
         if (ctrl.leave)
             ctrl.removeListen();
     };
+    // use keyboard arrow change scroll position
     this.changeScroll = function () {
         const main = document.getElementById(ctrl.mainId);
-        main.scroll(0, 100);
+        const element = main.children[ctrl.pointer];
+        main.scroll(0, element.offsetTop);
     };
+    // on select object
+    this.select = function (index) {
+        ctrl.input = ctrl.getView(ctrl.objects[index ? index : ctrl.pointer]);
+        ctrl.onSelect({obj: ctrl.objects[index ? index : ctrl.pointer]});
+        ctrl.pointer = -1;
+    };
+    //user use keyboard get keyCode and process
     this.keyDown = function (e) {
         switch (e.keyCode) {
             case 9:
@@ -50,13 +62,26 @@ costumeDropDown.controller('costumeDropDown', function ($scope) {
                 $scope.$apply();
                 break;
             case  13:
-                ctrl.input = ctrl.objects[ctrl.pointer][ctrl.view];
-                ctrl.pointer = -1;
-                ctrl.onSelect({obj: ctrl.objects[ctrl.pointer]});
+                ctrl.select();
                 ctrl.removeListen();
 
         }
     };
+    //on change text in text box with user
+    this.change = function () {
+        if (this.minActive && parseInt(this.minActive) <= this.input.length)
+            this.onChange({text: this.input});
+        else if (!this.minActive)
+            this.onChange({text: this.input});
+
+    };
+    //watch all input objects on change  reset options
+    $scope.$watchCollection(function () {
+            return ctrl.objects
+        },
+        function (newValue, oldValue) {
+            ctrl.pointer = 0;
+        });
 
 
 });
@@ -64,11 +89,19 @@ costumeDropDown.component('costumeDropDown', {
     templateUrl: "/Components/CostumeDropDown/index.html",
     controller: 'costumeDropDown',
     bindings: {
-        class: '@?',
-        name: '@?',
+        inputClass: '@?',
+        inputName: '@',
+        inputId:'@',
         objects: '<',
-        icons: '@?',
-        view: '@?',
-        onSelect: "&"
+        iconTypes: "@",
+        icons: '@',
+        imageRoot: '@',
+        imageType: '@',
+        viewName: '@',
+        getView: '=',
+        group: '@',
+        onSelect: "&",
+        onChange: '&',
+        minActive: "=?",
     }
 });
--- Routes/DropDown/index.js
+++ Routes/DropDown/index.js
@@ -1,9 +1,25 @@
-app.controller("DropDownController", ["$scope",'$http', function ($scope,$http) {
+app.controller("DropDownController", ["$scope", '$http', function ($scope, $http) {
     $http.get("/search.json").then(function (response) {
         $scope.Nationalities = response.data.Response.nationalities;
     });
- $scope.filter = function (text) {
+    $http.get("/s.json").then(function (response) {
+        $scope.dec = response.data;
+    });
+    $scope.view = function (obj) {
+        return obj.CountryName;
+    };
+    $scope.filter = function (text) {
+        $scope.objects = [];
+        for (let i = 0; i < $scope.Nationalities.length; i++)
+            if ($scope.Nationalities[i]['CountryName'].toLowerCase().indexOf(text) + 1)
+                $scope.objects.push($scope.Nationalities[i]);
 
- }
+    };
+    $scope.im = function (obj) {
+        return obj.CityName + ', ' + obj.CountryCode;
+    };
+    $scope.l = function (obj) {
+        console.log(obj);
+    }
 
 }]);
