1144e94ca09bb6e27f1ef14e518a63fe4c5141ff
amir <amirmohhamadsoltani@gmai.com>
2020-04-07 15:53:52 +0800

    create new component datepicker

---
+++ Components/DatePickerEn/index.js
@@ -0,0 +1,158 @@
+app.component('datePickerEn', {
+    transclude: true,
+    templateUrl: 'Components/DatePickerEn/index.html',
+    controller: function () {
+
+        this.weekDay = {M: ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa']};
+        this.start = 0;
+        this.first = null;
+        this.end = null;
+        this.hover = null;
+        this.type = 'M';
+        this.agent = 'desktop';
+        let tooltip = false;
+        const today = moment();
+        const convert = (1000 * 60 * 60 * 24);
+        this.monthsName = {
+            M: ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+        };
+        this.persianNumber = function (n) {
+            let str = '';
+            for (let i = 0; i < n.length; i++)
+                if (numbers[n[i]])
+                    str += numbers[n[i]];
+                else
+                    str += n[i];
+            return str;
+        };
+        this.$onInit = function () {
+            this.changeDate = this.changeDate?this.changeDate:false;
+            if (this.first)
+                this.getOneYearMonths(this.first);
+            else
+                this.getOneYearMonths(today);
+            this.desktop = {
+                M: [this.months.M[this.start], this.months.M[this.start + 1]]
+            };
+
+
+        };
+        this.months = { M: []};
+        this.getJDays = function (date) {
+            const days = [];
+            const weekDay = date.day() % 6;
+            for (let i = 0; i <= moment.jDaysInMonth(date.jYear(), date.jMonth()) + weekDay; i++)
+                days.push({
+                    text: weekDay >= i ? '' : this.persianNumber((i - weekDay).toString()),
+                    data: weekDay >= i ? null : moment(date.format('jYYYY/jMM/' + (i - weekDay)), "jYYYY/jMM/jDD")
+                });
+            return days;
+        };
+        this.getDays = function (date) {
+            const days = [];
+            const weekDay = date.day();
+            for (let i = 1; i <= date.daysInMonth() + weekDay; i++)
+                days.push({
+                    text: weekDay >= i ? '' : i - weekDay,
+                    data: weekDay >= i ? null : moment(date.format('YYYY/MM/' + (i - weekDay)), "YYYY/MM/DD")
+                });
+            return days;
+        };
+        this.getOneYearMonths = function (firstMonth) {
+            const month = moment(firstMonth);
+            const Jmonth = moment(firstMonth);
+            this.months = {J: [], M: []};
+            for (let i = 0; i < 12; i++) {
+                month.startOf('month');
+                this.months.M.push({name: month.month(), days: this.getDays(month)});
+                month.add(1, 'Month');
+            }
+        };
+
+
+        this.change_start = function (index) {
+            if (index)
+                this.start += 1;
+            else
+                this.start -= 1;
+            this.desktop = {
+                M: [this.months.M[this.start], this.months.M[this.start + 1]]
+            }
+        };
+        this.onHover = function (data, event) {
+            if (data.day.text === '')
+                return false;
+            if (tooltip) {
+                tooltip.remove();
+                tooltip = false;
+                this.hover = null;
+                return true;
+            }
+            const target = event.target;
+            const offset = $(target).offset();
+            tooltip = document.createElement('span');
+            tooltip.className = "date-picker-tooltip";
+            if (this.first === null)
+                tooltip.innerHTML = "start in";
+            else if (this.end === null)
+                tooltip.innerHTML = `return ${Math.floor((data.day.data - this.first) / convert)} days later`;
+            else if (data.day.data > this.first && data.day.data < this.end)
+                tooltip.innerHTML = `after ${Math.floor((data.day.data - this.first) / convert)} days`;
+            document.body.append(tooltip);
+            tooltip.style.left = offset.left + $(target).width() / 2 + 'px';
+            tooltip.style.top = offset.top - tooltip.offsetHeight - 12 + 'px';
+            this.hover = data.day.data;
+
+        };
+        this.select = function (data) {
+
+            const date = data.day.data;
+            if (date < today - convert)
+                return '';
+            else if (this.first === null)
+                this.first = data.day.data;
+            else if (this.end === null && Math.floor((data.day.data - this.first) / convert) <= 30 && this.first < date) {
+                this.end = data.day.data;
+                this.onSelect(this.first, this.end);
+            } else if (this.end !== null && date > today - convert) {
+                this.first = date;
+                this.end = null;
+            }
+        };
+        this.getClass = function (data) {
+            const date = data.day.data;
+            if (data.day.text === '' || date === null)
+                return '';
+            else if (this.first !== null && date.year() === this.first.year() && date.month() === this.first.month() &&
+                date.date() === this.first.date())
+                return 'date-picker-day-first';
+            else if ((this.first !== null) && ((date > this.first && date < this.end)
+                || (this.end === null && date > this.first && date < this.hover)))
+                return 'date-picker-day-mid';
+            else if ((this.end !== null && date.year() === this.end.year() && date.month() === this.end.month() &&
+                date.date() === this.end.date() || (this.hover === date && this.end === null)) && this.first !== null)
+                return 'date-picker-day-end';
+            else if (date < today - convert || ((this.first && Math.floor((date - this.first) / convert)) > 30 || this.first > date) && this.end === null)
+                return "date-picker-day-hatchet"
+
+        }
+        this.$update = function(values){
+            if(values.checkIn)
+                this.first = this.checkIn;
+            if(values.checkOut)
+                this.end = this.checkOut;
+        }
+
+
+    },
+    bindings: {
+        checkIn: '<',
+        checkOut: '<',
+        agent:'=',
+        onSelect: '=',
+        title:"<",
+        onclose:"&",
+        acceptButton : "<",
+        onClose:'&',
+    }
+});
